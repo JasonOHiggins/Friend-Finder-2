4aec1e62017783280feabd4bb0366265a151240f
Jason O'Higgins <jason.ohiggins@gmail.com>
2018-07-29 21:47:58 -0500

    final changes
    final updates to survey.html
    modal now functional

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,35 +1,3 @@
-// var friends = require("../data/friends.js");
-
-// function userDiff(a,b) {
-//   'use strict';
-
-//   return a.scores.reduce((acc, curr, idx) => {
-//     return acc + Math.abs(curr - b.scores[idx]);
-//   }, 0);
-// }
-
-// module.exports = function(app) { ;
-//   "use strict";
-
-//   app.get("/api/friends", function(req, res) {
-//     res.json(friends);
-//   });
-
-//   app.post("/api/friends", function (req, res) {
-//     res.send(
-//       friends[
-//       friends.map((friend, i) => {
-//         return {
-//           'diff': userDiff(req.body, friend),
-//           'index': i
-//         };
-//       }).sort((a, b) => a.diff - b.diff)[0].index
-//       ]
-//     );
-//   });
-// };
-
-
 
 var friends = require('../data/friends.js');
 
