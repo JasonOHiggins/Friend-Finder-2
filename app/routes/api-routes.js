d2b1b99327c7a596561b2c25473699ed5028019b
Jason O'Higgins <jason.ohiggins@gmail.com>
2018-07-29 21:33:59 -0500

    Changes to SurveyHTML
    added questions to this instead of separate survey.js file.

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,10 +1,83 @@
-var friends = require("../data/friends.js");
+// var friends = require("../data/friends.js");
 
-module.exports = function(app) {
+// function userDiff(a,b) {
+//   'use strict';
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
-  
-}
+//   return a.scores.reduce((acc, curr, idx) => {
+//     return acc + Math.abs(curr - b.scores[idx]);
+//   }, 0);
+// }
 
+// module.exports = function(app) { ;
+//   "use strict";
+
+//   app.get("/api/friends", function(req, res) {
+//     res.json(friends);
+//   });
+
+//   app.post("/api/friends", function (req, res) {
+//     res.send(
+//       friends[
+//       friends.map((friend, i) => {
+//         return {
+//           'diff': userDiff(req.body, friend),
+//           'index': i
+//         };
+//       }).sort((a, b) => a.diff - b.diff)[0].index
+//       ]
+//     );
+//   });
+// };
+
+
+
+var friends = require('../data/friends.js');
+
+module.exports = (app) => {
+
+    app.get('/api/friends', (req, res) => {
+        res.json(friends);
+    });
+
+    app.post('/api/friends', (req, res) => {
+
+        var newFriend = req.body;
+
+        var bestMatch = {};
+
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            if (newFriend.scores[i] == "1 (Strongly Disagree)") {
+                newFriend.scores[i] = 1;
+            } else if (newFriend.scores[i] == "5 (Strongly Agree)") {
+                newFriend.scores[i] = 5;
+            } else {
+                newFriend.scores[i] = parseInt(newFriend.scores[i]);
+            }
+        }
+
+        var bestMatchIndex = 0;
+
+        var bestMatchDifference = 40;
+
+        for (var i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var differenceOneScore = Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+                totalDifference += differenceOneScore;
+            }
+
+            if (totalDifference < bestMatchDifference) {
+                bestMatchIndex = i;
+                bestMatchDifference = totalDifference;
+            }
+        }
+
+        bestMatch = friends[bestMatchIndex];
+
+        friends.push(newFriend);
+
+        res.json(bestMatch);
+    });
+
+};
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var express = require("express");
+var express = require("express");          
+var app = express();                        
+var bodyParser = require("body-parser");    
+var path = require("path");                 
 
-var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 8080;        
 
-var app = express();
+var friends = require('./app/data/friends.js');  
 
-var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
+app.use(express.static("app/public"));              
+app.use(bodyParser.text());                     
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+require("./app/routes/api-routes.js")(app);    
+require("./app/routes/html-routes.js")(app);  
 
-app.use(express.static("public"));
-
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT, function() {                       
+  console.log("App listening on PORT: " + PORT);   
+});                                                 
